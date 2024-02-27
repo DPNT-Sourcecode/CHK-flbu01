9ef2125c559a5156990972174bfd87fe7662d97a
sbx_user1051 <sbx_user1051@169.254.28.61>
2024-02-27 20:22:01 +0000

    Tue Feb 27 20:22:01 UTC 2024

---
+++ src/BeFaster.App.Tests/Solutions/HLO/HelloSolutionTest.cs
@@ -0,0 +1,19 @@
+﻿using BeFaster.App.Solutions.HLO;
+using BeFaster.App.Solutions.SUM;
+using FluentAssertions;
+
+namespace BeFaster.App.Tests.Solutions.SUM
+{
+    public class HelloSolutionTest
+    {
+        [Fact]
+        public void Hello()
+        {
+            //Arrange
+            var result = HelloSolution.Hello("string");
+
+            //Assert
+            result.Should().Be("Hello world!");
+        }
+    }
+}
